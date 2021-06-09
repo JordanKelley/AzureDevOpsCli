87bfd5b1d70e4fe0985d3a6add5288946e6a8d67
Jordan Kelley <jordan.kelley105@gmail.com>
2021-06-09 13:49:24 -0500

    added creation of repo

--- Scaffold.ps1
+++ Scaffold.ps1
@@ -14,5 +14,22 @@ function createProject{
     return $project.id
 }
 
+function createRepo{
+    param(
+        [String]$org,
+        [String]$projectID,
+        [String]$repoName
+    )
+
+    Write-Host "`nCreating repository with name $($repoName) . . . " 
+    $repo = az repos create --org $org -p $projectID --name $repoName -o json | ConvertFrom-Json
+    Write-Host "Created repository with name $($repo.name) and Id $($repo.id)"
+    return $repo.id
+}
+
+$org = 'https://dev.azure.com/jordankelley105/'
+
 # scaffolding
-createProject -org 'https://dev.azure.com/jordankelley105/' -projectName 'TestingScript' -process 'Agile' -sourceControl 'git' -visibility 'private'
+$projectId = createProject -org $org -projectName 'TestingProject' -process 'Agile' -sourceControl 'git' -visibility 'private'
+
+createRepo -org $org -projectID $projectId -repoName 'TestingRepo'
